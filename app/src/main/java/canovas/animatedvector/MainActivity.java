07d4929332d7bafc5831f8896710ef4fc6eb2eec
Christelle Canovas <christelle.canovas@gmail.com>
2020-03-23 18:34:03 +0100

    Translate animation to right eye added

--- app/src/main/java/canovas/animatedvector/MainActivity.java
+++ app/src/main/java/canovas/animatedvector/MainActivity.java
@@ -2,7 +2,9 @@ package canovas.animatedvector;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Animatable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView robot_imgview = findViewById(R.id.main_robot_imgview);
+
+        Animatable animatable = (Animatable) robot_imgview.getDrawable();
+        animatable.start();
+
+
     }
 }
